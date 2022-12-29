c525c4e8726357053c0f2c8578c2bf1083db148d
Carlos Moraes <113129034+Carlos-Moraes2@users.noreply.github.com>
2022-12-28 23:17:59 -0300

    projeto final

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 const filterElement = document.querySelector('header input')
-
 const cards = document.querySelectorAll('.cards li')
 
 filterElement.addEventListener('input', filterCards)
-// filter function
+
 function filterCards() {
-    // if the filter is not empty
-    if(filterElement.value != '') {
-                // for each card of cards 
-        for (let card of cards) {
-        // get card heading (title)
-        let title = card.querySelector('h2')
-        // tranform to lower case
+  if(filterElement.value !== '') {
+    for (let card of cards) {
+      let title = card.querySelector('h2')
         title = title.textContent.toLowerCase()
-        console.log(title)
-        // transform filter text to lower case
-        // if card title does not include the filter text
-          // hide the card element
-        // else
-          // unhide the card element
-    // else
-      // for each card of cards
-        // unhide the card element
+        let filterText = filterElement.value.toLowerCase()
+        if(!title.includes(filterText)) {
+          card.style.display = "none"
+        }
+
+        else {
+          card.style.display = "block"
+        }
+        
       }
     } else {
-    // for each card of cards
-        // geat card heasdding
-        //transform to lower case 
+      for (let card of cards) {
+      card.style.display = "block"
     }
+  }
 }
